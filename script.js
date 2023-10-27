b1653219158f167778b672cbad71fece12e91983
prithvirajbarad <112259618+prithvirajbarad@users.noreply.github.com>
2023-10-27 10:14:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,49 +1,157 @@
-function setCookie(name, value, days) {
-  const expires = new Date(Date.now() + days * 24 * 60 * 60 * 1000).toUTCString();
-  document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/`;
+const questions = [
+  {
+    question: "What is the capital of France?",
+    choices: ["Paris", "London", "Berlin", "Madrid"],
+    answer: "Paris",
+  },
+  {
+    question: "What is the highest mountain in the world?",
+    choices: ["Everest", "Kilimanjaro", "Denali", "Matterhorn"],
+    answer: "Everest",
+  },
+  {
+    question: "What is the largest country by area?",
+    choices: ["Russia", "China", "Canada", "United States"],
+    answer: "Russia",
+  },
+  {
+    question: "Which is the largest planet in our solar system?",
+    choices: ["Earth", "Jupiter", "Mars"],
+    answer: "Jupiter",
+  },
+  {
+    question: "What is the capital of Canada?",
+    choices: ["Toronto", "Montreal", "Vancouver", "Ottawa"],
+    answer: "Ottawa",
+  },
+];
+
+// Check if session storage is supported
+function isSessionStorageSupported() {
+  try {
+    return window.sessionStorage !== null;
+  } catch (e) {
+    return false;
+  }
 }
 
-// Function to get a cookie by name
-function getCookie(name) {
-  const cookies = document.cookie.split(';');
-  for (const cookie of cookies) {
-    const [cookieName, cookieValue] = cookie.trim().split('=');
-    if (cookieName === name) {
-      return decodeURIComponent(cookieValue);
+// Check if local storage is supported
+function isLocalStorageSupported() {
+  try {
+    return window.localStorage !== null;
+  } catch (e) {
+    return false;
+  }
+}
+
+// Retrieve progress from session storage
+function retrieveProgress() {
+  if (isSessionStorageSupported()) {
+    const progress = sessionStorage.getItem('progress');
+    if (progress) {
+      return JSON.parse(progress);
     }
   }
-  return null;
+  return [];
 }
 
-// Function to apply saved preferences
-function applyPreferences() {
-  const fontSize = getCookie('fontsize');
-  const fontColor = getCookie('fontcolor');
-
-  if (fontSize) {
-    document.documentElement.style.setProperty('--fontsize', fontSize + 'px');
-    document.getElementById('fontsize').value = fontSize;
-  }
-
-  if (fontColor) {
-    document.documentElement.style.setProperty('--fontcolor', fontColor);
-    document.getElementById('fontcolor').value = fontColor;
+// Save progress in session storage
+function saveProgress() {
+  if (isSessionStorageSupported()) {
+    const choices = document.querySelectorAll('input[type="radio"]:checked');
+    const progress = Array.from(choices).map((choice) => choice.value);
+    sessionStorage.setItem('progress', JSON.stringify(progress));
   }
 }
 
-// Event listener for form submission
-document.querySelector('form').addEventListener('submit', function (e) {
-  e.preventDefault();
+// Retrieve score from local storage
+function retrieveScore() {
+  if (isLocalStorageSupported()) {
+    const score = localStorage.getItem('score');
+    if (score) {
+      return parseInt(score);
+    }
+  }
+  return 0;
+}
 
-  const fontSize = document.getElementById('fontsize').value;
-  const fontColor = document.getElementById('fontcolor').value;
+// Save score in local storage
+function saveScore(score) {
+  if (isLocalStorageSupported()) {
+    localStorage.setItem('score', score.toString());
+  }
+}
 
-  // Set CSS variables and save preferences in cookies
-  document.documentElement.style.setProperty('--fontsize', fontSize + 'px');
-  document.documentElement.style.setProperty('--fontcolor', fontColor);
-  setCookie('fontsize', fontSize, 30);
-  setCookie('fontcolor', fontColor, 30);
-});
+// Display the quiz questions and choices
+function renderQuestions() {
+  const questionsElement = document.getElementById('questions');
+  const progress = retrieveProgress();
 
-// Apply preferences on page load
-applyPreferences();
+  for (let i = 0; i < questions.length; i++) {
+    const question = questions[i];
+    const questionElement = document.createElement('div');
+    const questionText = document.createTextNode(question.question);
+    questionElement.appendChild(questionText);
+
+    for (let j = 0; j < question.choices.length; j++) {
+      const choice = question.choices[j];
+      const choiceElement = document.createElement('input');
+      choiceElement.setAttribute('type', 'radio');
+      choiceElement.setAttribute('name', `question-${i}`);
+      choiceElement.setAttribute('value', choice);
+
+      if (progress[i] === choice) {
+        choiceElement.setAttribute('checked', true);
+      }
+
+      choiceElement.addEventListener('change', saveProgress);
+
+      const choiceText = document.createTextNode(choice);
+      const choiceLabel = document.createElement('label');
+      choiceLabel.appendChild(choiceElement);
+      choiceLabel.appendChild(choiceText);
+
+      questionElement.appendChild(choiceLabel);
+    }
+
+    questionsElement.appendChild(questionElement);
+  }
+}
+
+// Calculate the score
+function calculateScore() {
+  const choices = document.querySelectorAll('input[type="radio"]:checked');
+  let score = 0;
+
+  for (let i = 0; i < choices.length; i++) {
+    const choice = choices[i].value;
+    const answer = questions[i].answer;
+
+    if (choice === answer) {
+      score++;
+    }
+  }
+
+  return score;
+}
+
+// Display the score
+function displayScore(score) {
+  const scoreElement = document.getElementById('score');
+  const scoreText = document.createTextNode(`Your score is ${score} out of ${questions.length}.`);
+  scoreElement.appendChild(scoreText);
+}
+
+// Handle the submit button click event
+function handleSubmit() {
+  const score = calculateScore();
+  saveScore(score);
+  displayScore(score);
+}
+
+// Attach event listener to the submit button
+const submitButton = document.getElementById('submit');
+submitButton.addEventListener('click', handleSubmit);
+
+// Render the questions on page load
+renderQuestions();
